3cc5566f603e16aae5ffc53f00158ef31f96471b
Amos Koech <koechronix@yahoo.com>
2022-06-05 21:11:53 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+function moveDodgerLeft(){
+    const rightNumbers =dodger.style.right.replace("px", "");
+    const right=parseInt(rightNumbers,10)
+    if (right <0)
+    if (right <0 && left >360 ){
+        dodger.style.right = `${left - 1}px`;
+    }
+        
+    }
+
+
+
+
+
+
+
+
+function moveDodgerRight(){
+    const leftNumbers =dodger.style.left.replace("px", "");
+    const left= parseInt(leftNumbers,10);
+    if (left > 0){
+    if (left > 0 && left < 360 ){
+        dodger.style.left = `${left + 1}px`;
+    };
+    }
+
+}
+
