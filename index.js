04602be07aa69bf2ce7b5b5f1ae0e369a38d40bc
Amos Koech <koechronix@yahoo.com>
2022-06-13 11:31:49 -0400

    completed dodger

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 // Your code here
-function moveDodgerLeft(){
-    const rightNumbers =dodger.style.right.replace("px", "");
-    const right=parseInt(rightNumbers,10)
-    if (right <0)
-    if (right <0 && left >360 ){
-        dodger.style.right = `${left - 1}px`;
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
     }
-        
-    }
+  }
 
 
 
